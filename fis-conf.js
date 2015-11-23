9a4358f6f1726f7834fa009b18ef57c781a5937b
Peach <scdzwyxst@gmail.com>
2015-11-23 16:09:09 +0800

    Modify the configuration of fis to remove the useless configurations

--- fis-conf.js
+++ fis-conf.js
@@ -1,17 +1,5 @@
-// default settings. fis3 release
-
-// Global start
 fis.match('src/ui-p2p-lending.less', {
   parser: fis.plugin('less'),
-  rExt: '.css'
+  rExt: '.css',
+  release: 'dist/ui-p2p-lending.css'
 })
-
-fis.match('src/*.less', {
-  packTo: 'dist/ui-p2p-lending.css'
-})
-
-// Global end
-
-// default media is `dev`
-
-// extends GLOBAL config
